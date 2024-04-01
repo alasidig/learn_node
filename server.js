9b61d4fb90ad3bd6a63d4eeee2ad0be3af5e083a
alasidig <ala.sidig@gmail.com>
2024-04-01 13:47:58 +0300

    Step 3 basic routing

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200,    { 'Content-Type': 'text/html' });
-  res.end('<h1>Welcome to Tasks Management</h1>');
+    console.log(req.url);
+    
+  switch (req.url) {
+    case '/':
+      res.writeHead(200,    { 'Content-Type': 'text/html' });
+      res.end('<h1>Welcome to Tasks Management</h1>');
+      break;
+    case '/task':
+      res.writeHead(200,    { 'Content-Type': 'text/html' });
+      res.end('<h1>Details of a task goes here</h1>');
+      break;
+    case '/new':
+      res.writeHead(200,    { 'Content-Type': 'text/html' });
+      res.end('<h1>Task form will be here</h1>');
+      break;
+    default:
+      res.writeHead(404,    { 'Content-Type': 'text/html' });
+      res.end('<h1>Not found</h1>');
+  }
 });
 
 const port =  3000;
