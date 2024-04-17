d5772f5f361cfb995dde9d82f1a45a642f457175
alasidig <ala.sidig@gmail.com>
2024-04-17 07:56:50 +0300

    Step 19 Consume Rest API using Fetch

--- public/frontendJs.js
+++ public/frontendJs.js
@@ -1,9 +1,33 @@
-function deleteTask(taskId) {
+async function deleteTask(taskId) {
     if (confirm('Are you sure you want to delete this task?')) {
         console.log(`Deleting task with id: ${taskId}`);
+        const response = await fetch(`/task/${taskId}`, {method: 'DELETE'});
+        if (response.ok) {
+            document.location.href = '/';
+        } else {
+            alert(`Failed to delete task with id ${taskId}`);
+        }
     }
 }
 
-function markDone(taskId) {
+async function markDone(taskId) {
     console.log(`Task with id ${taskId} marked as done`);
+
+    const response = await fetch(`/task/${taskId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({done: true})
+    });
+
+    if (response.ok) {
+        const taskDiv = document.querySelector('.task');
+        taskDiv.classList.remove('pending')
+        taskDiv.classList.add('done');
+        const doneButton = taskDiv.querySelector('.done-button');
+        doneButton.disabled = true;
+    } else {
+        alert('Failed to mark task as done');
+    }
 }
